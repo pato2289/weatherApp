9f705a23079e92f77b3829c29318f3a17f2387fc
patobordenave <patriciofbordenave@gmail.com>
2019-12-04 15:55:26 -0300

    Updates

---
+++ precache-manifest.768d0b88ef23e9f4b2ed7ebae2af335f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153688d8f006cf11a1a5005d646e4b06",
+    "url": "/weatherApp/index.html"
+  },
+  {
+    "revision": "92384f4ba5031f07c0a2",
+    "url": "/weatherApp/static/css/main.3751ae63.chunk.css"
+  },
+  {
+    "revision": "103854798b187568c9bb",
+    "url": "/weatherApp/static/js/2.d428223a.chunk.js"
+  },
+  {
+    "revision": "92384f4ba5031f07c0a2",
+    "url": "/weatherApp/static/js/main.7d20cbd8.chunk.js"
+  },
+  {
+    "revision": "1a6ef520985b2b4a4b26",
+    "url": "/weatherApp/static/js/runtime-main.9d69a001.js"
+  }
+]);
